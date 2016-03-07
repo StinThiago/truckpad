d59def507694d1a8be9fca6db398c2dffdca7d79
stin.thiago@gmail.com <stin.thiago@gmail.com>
2016-03-07 19:22:09 -0300

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   #resources :users
+
+  #root "todo_lists#index"
+  root :to => "home#index" #Controller#Action
 
   resources :todo_lists do 
       resources :todo_items do
-      	member do
-    		patch :complete
-    	end
-  	end
+        member do
+        patch :complete
+      end
+    end
   end
-
-  root "todo_lists#index"
 
 end
